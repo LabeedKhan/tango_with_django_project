6eb220ef1c7c3b2f94b17a26979af47b7431391f
Labeed Khan <2377270k@student.gla.ac.uk>
2019-01-22 17:07:23 +0000

    New commit for rango image

--- rango/views.py
+++ rango/views.py
@@ -4,9 +4,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says Hello")
-
-def index(request):
     context_dict= {'boldmessage': "Crunchy, creamy, cookie"}
     return render(request, 'rango/index.html', context=context_dict)
 
+def about(request):
+    return HttpResponse("<a href='/rango/about/'>About</a><a href= /rango/>Index</a> "  )
