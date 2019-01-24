a2bd260c9d30c8f68725bddd950e6bf831ac476a
Labeed Khan <2377270k@student.gla.ac.uk>
2019-01-24 19:38:43 +0000

    Populatefileschanges

--- rango/models.py
+++ rango/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return self.name
 
@@ -17,3 +20,5 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
+
--- rango/views.py
+++ rango/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    context_dict= {'boldmessage': "Crunchy, creamy, cookie"}
+    context_dict= {'boldmessage': "Crunchy,creamy,cookie,candy,cupcake!"}
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
